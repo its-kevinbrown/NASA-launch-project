6c9f544027f1be0b22849a98036b1efdcf74c76c
its-kevinbrown <ninobrizzzy@gmail.com>
2023-02-21 10:17:40 +0100

    Nasa First Commit

---
+++ server/src/routes/planets/planets.router.js
@@ -0,0 +1,11 @@
+const express = require('express');
+
+const {
+    httpGetAllPlanets
+} = require ('./planets.controllers');
+
+const planetsRouter = express.Router();
+
+planetsRouter.get('/', httpGetAllPlanets);
+
+module.exports = planetsRouter;
